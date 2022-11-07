696014ea8f0ba870c149d780bd19f167ca7b8e09
Jasper Alblas <just_jasper@hotmail.com>
2022-11-07 21:11:26 +0100

    Morgan added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
+const morgan = require('morgan');
+
 const app = express()
 
 app.use(express.json())
+
+morgan.token('body', (req, res) => JSON.stringify(req.body));
+app.use(morgan(':method :url :status :response-time ms - :res[content-length] :body - :req[content-length]'));
 
 let persons = [
     {
@@ -62,7 +67,7 @@ app.post('/api/persons', (request, response) => {
 
     if (!body.name || !body.number) {
         return response.status(400).json({
-            error: 'name or number missing'
+            error: 'name and/or number missing'
         })
     }
 
