fcbd1150bf188a1139547dcba85acc40f785486f
Jasper Alblas <just_jasper@hotmail.com>
2022-11-10 09:49:55 +0100

    mongo script edited so now users can be added and listed

--- mongo.js
+++ mongo.js
@@ -1,13 +1,11 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length < 3) {
-    console.log('Please provide the password as an argument: node mongo.js <password>')
+if (process.argv.length < 3 || process.argv.length > 5) {
+    console.log('Please provide the password, name and number as an argument: node mongo.js <password> <name> <number>')
+    console.log('OR')
+    console.log('Only provide the password as an argument: node mongo.js <password>')
     process.exit(1)
 }
-
-const password = process.argv[2]
-
-const url = `mongodb+srv://jasper:${password}@cluster1.gmcixyl.mongodb.net/phonebook?retryWrites=true&w=majority`
 
 const personSchema = new mongoose.Schema({
     name: String,
@@ -16,20 +14,49 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-mongoose
-    .connect(url)
-    .then((result) => {
-        console.log('connected')
+const password = process.argv[2]
+const url = `mongodb+srv://jasper:${password}@cluster1.gmcixyl.mongodb.net/phonebook?retryWrites=true&w=majority`
 
-        const person = new Person({
-            name: 'Agent 13',
-            number: '1234-5678'
+if (process.argv.length == 3) {
+    mongoose
+        .connect(url)
+        .then((result) => {
+            console.log('connected')
+            Person
+                .find({})
+                .then(persons => {
+                    console.log("Phonebook contents:")
+                    persons.forEach(person => {
+                        console.log(`${person.name} - ${person.number}`)
+                    })
+                    mongoose.connection.close()
+                })
+
         })
 
-        return person.save()
-    })
-    .then(() => {
-        console.log('person saved!')
-        return mongoose.connection.close()
-    })
-    .catch((err) => console.log(err))
+
+} else if (process.argv.length == 5) {
+    const name = process.argv[3]
+    const number = process.argv[4]
+
+    mongoose
+        .connect(url)
+        .then((result) => {
+            console.log('connected')
+
+            const person = new Person({
+                name: name,
+                number: number
+            })
+
+            return person.save()
+        })
+        .then((person) => {
+            console.log(`${person.name} added to the phonebook with number: ${person.number}`)
+            return mongoose.connection.close()
+        })
+        .catch((err) => console.log(err))
+}
+
+
+
